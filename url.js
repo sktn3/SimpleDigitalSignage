1ebec1dd208aac38400d10d1d3b3a4890affd338
sktn3 <34881336+sktn3@users.noreply.github.com>
2022-02-22 14:25:31 +0900

    upload files

---
+++ url.js
@@ -0,0 +1,11 @@
+show([
+{"url":"https://weathernews.jp/onebox/tenki/aichi/23108/", "interval":15},
+{"url":"https://www.nikkei.com/", "interval":20},
+{"url":"https://www.nikkei.com/markets/kabu/", "interval":30},
+{"url":"https://www3.nhk.or.jp/news/special/coronavirus/latest-news/", "interval":30},
+{"url":"https://www3.nhk.or.jp/news/", "interval":20},
+{"url":"https://www.sankei.com/", "interval":20},
+{"url":"https://jp.reuters.com/", "interval":20},
+{"url":"https://www.cnn.co.jp/", "interval":20},
+{"url":"https://nikkeiyosoku.com/", "interval":20}
+], 0);
